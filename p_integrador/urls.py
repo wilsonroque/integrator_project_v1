42b379b6391e8565d9f1fc6e82764e9375037200
wilsonroque <wilson.novidata@gmail.com>
2021-11-02 19:40:30 +0000

    acertado as views

--- p_integrador/urls.py
+++ p_integrador/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, about_page, contact_page
-from .views import home_page
+from django.urls import path
+from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     path('', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
-]
+    ]
