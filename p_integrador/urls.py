624fbc1d285af3cf8e5b6045c73f75c181d552bc
wilsonroque <wilson.novidata@gmail.com>
2021-11-02 17:59:28 +0000

    adicionando no urls.py about pages e contact pages

--- p_integrador/urls.py
+++ p_integrador/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, about_page, contact_page
 from .views import home_page
 
 urlpatterns = [
     path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
